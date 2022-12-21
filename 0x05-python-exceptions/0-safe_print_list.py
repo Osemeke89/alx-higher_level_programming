4ad88f021a7c862f6174029b50b7dc4db173606e
Osemeke Onyekachukwu <52927146+Osemeke89@users.noreply.github.com>
2022-12-21 10:36:50 -0800

    commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-
-
 def safe_print_list(my_list=[], x=0):
-    i = 0
-    printed = 0
+    index = 0
     for i in range(0, x):
         try:
-            print("{}".format(my_list[i]), end="")
-            printed += 1
+            print("{:d}".format(my_list[i]), end="")
+            index += 1
         except:
-            continue
+            break
     print()
-    return printed
+    return(index)
