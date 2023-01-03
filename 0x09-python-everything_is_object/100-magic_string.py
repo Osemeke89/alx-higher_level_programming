3d6080e927affbff467915f20c9a3600214daa7b
Osemeke Onyekachukwu <52927146+Osemeke89@users.noreply.github.com>
2023-01-03 13:29:33 -0800

    commit

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
 def magic_string():
-    magic_string.n = getattr(magic_string, 'n', 0) + 1
-    return ("Holberton, " * (magic_string.n - 1) + "Holberton")
+    from counter import Counter
+    Counter.i += 1
+    return ", ".join(["Holberton" for i in range(0, Counter.i)])
